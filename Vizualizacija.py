841492c2ed2614bcdbf3435a790fed54ac02808a
evelinamon <evelinamon0616@gmail.com>
2019-12-05 17:17:24 +0200

    Universitetas Pakeiciau vizualizacijos faile. Sukuriau createcomboBox metoda ir pakeiciau onChanged metoda

--- Vizualizacija.py
+++ Vizualizacija.py
@@ -8,50 +8,50 @@ class Pagrindinis_langas(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.qlabel = QLabel(self)
+        self.comboBox = self.createComboBox()
+        self.formaLabel = QLabel(self)
         self.initUI()
 
     def initUI(self):
 
-        self.formaLabel = QLabel(self)
         self.formaLabel.setText("Registracijos forma")
         self.formaLabel.move(100, 0)
-
-        self.comboBox()
 
         self.setGeometry(300, 300, 300, 300)
         self.setWindowTitle('Fakultetas')
         self.show()
 
-    def comboBox(self):
+    def createComboBox(self):
         combo = QComboBox(self)
         combo.addItem("---")
         combo.addItem("Studentai")
         combo.addItem("Destytojai")
         combo.move(97, 40)
 
-        self.qlabel = QLabel(self)
-        self.qlabel.move(10, 70)
-
-
         combo.activated[str].connect(self.onChanged)
-
+        return combo
     def onChanged(self):
-        if self.comboBox().currentIndex() == 0:
+        self.qlabel.move(10, 70)
+        if self.comboBox.currentIndex() == 0:
             pass
-        elif self.comboBox().currentIndex() == 1:
+        elif self.comboBox.currentIndex() == 1:
             self.qlabel.setText("Studiju programa")
             self.qlabel.adjustSize()
+            return self.qlabel
         else:
             self.qlabel.setText("Pareiga")
             self.qlabel.adjustSize()
+            return self.qlabel
+
 
 
             #self.qline = QLineEdit(self)
             #self.qline.move(100, 70)
             #self.qline.setText("labas")
-            #self.qline.adjustSize()
+            #self.qline.adjustSize()0
 
-    def closeEvent(self, event):
+    """def closeEvent(self, event):
 
         reply = QMessageBox.question(self, 'Message',
                                      "Are you sure to quit?", QMessageBox.Yes |
@@ -60,7 +60,7 @@ class Pagrindinis_langas(QWidget):
         if reply == QMessageBox.Yes:
             event.accept()
         else:
-            event.ignore()
+            event.ignore()"""
 
 
 if __name__ == '__main__':
