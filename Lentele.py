fce8767edad2eafdada0d55116f8bafcafe174b9
evaldmonastyrski <evaldmon@gmail.com>
2019-12-07 18:50:03 +0200

    lenteles uzpildymas

--- Lentele.py
+++ Lentele.py
@@ -18,29 +18,17 @@ class Issokantis_langas(QWidget):
         self.show()
 
     def createTable(self):
-
         self.tableWidget.setRowCount(4)
         self.tableWidget.setColumnCount(4)
         self.tableWidget.setHorizontalHeaderLabels(["Vardas", "Pavarde", "Studiju programa", "Kursas"])
 
-        vardas = ""
-        pavarde = ""
-        studiju_programa = ""
-        kursas = ""
         row = 0
-        column = 0
+        for studentas in self.sarasas:
+            column = 0
+            self.tableWidget.setItem(row, column, QTableWidgetItem(studentas.gauti_varda()))
+            self.tableWidget.setItem(row, column + 1, QTableWidgetItem(studentas.gauti_pavarde()))
+            self.tableWidget.setItem(row, column + 2, QTableWidgetItem(studentas.studiju_programa))
+            self.tableWidget.setItem(row, column + 3, QTableWidgetItem(studentas.kursas))
+            row = row + 1
 
-        for studentai in self.sarasas:
-            vardas = studentai.gauti_varda()
-            pavarde = studentai.gauti_pavarde()
-            studiju_programa = studentai.studiju_programa
-            kursas = studentai.kursas
-            for elementai in range(4, 0, -1):
-                row = row + 1
-                column = 0
-
-        self.tableWidget.setItem(row, column, QTableWidgetItem(vardas))
-        self.tableWidget.setItem(row, column+1, QTableWidgetItem(pavarde))
-        self.tableWidget.setItem(row, column+2, QTableWidgetItem(studiju_programa))
-        self.tableWidget.setItem(row, column+3, QTableWidgetItem(kursas))
         self.tableWidget.move(0, 0)
